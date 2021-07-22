e405c85b67513d0a5cdaa029f118c1bd03e4f4cf
AlexanderKonushin <87746348+AlexanderKonushin@users.noreply.github.com>
2021-07-22 09:45:29 +0700

    Update PlayerScript.cs

--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -1,18 +1,19 @@
 using System.Collections;
-using System.Collections.Generic;
+using System.Collections.Generic;  // TODO: не нужные библиотеки желательно удалять
 using UnityEngine;
 
-public class PlayerScript : MonoBehaviour
+public class PlayerScript : MonoBehaviour // TODO: имя класса должно более чётко описывать свою задачу. Если правильно понял, этот скрипт управляет объектом Player. Стоит назвать его PlayerController.
 {
     //Prefabs
-    public GameObject roadPrefab_;
+    public GameObject roadPrefab_;  // TODO: 1. Если это префаб, то нужно сделать roadPrefab префабом и указывать ссылку на него. У тебя просто ссылка на объект на сцене - это не префаб.
+                                   //       2. Поля класса следует помечать модификатором доступа private, а если ты хочешь к ним обратиться, то надо использовать [SerializeField].
 
     //Preferences
-    public float changeSideCooldownSec_;
+    public float changeSideCooldownSec_;  // TODO: То же самое про private и не стоит в конце ставить нижний слеш.
     public float fullChangeTime_;
     public float playerWidth_;
 
-    private enum eSide : int { 
+    private enum eSide : int {  // TODO: Не обязательно наследовать от int. Первый символ должен быть заглавным. Лучше этот enum вообще вынести в отдельный файл, что бы не загрязнять класс.
         bottom = 1,
         left = 2,
         right = 3,
@@ -20,12 +21,12 @@ public class PlayerScript : MonoBehaviour
     }
 
     private eSide side_ = eSide.bottom;
-    private float NextSideChange_;
+    private float NextSideChange_;  // TODO: в большинстве компаний поддерживаются стандарты microsoft: private со строчной буквы, public с заглавной.
     private Vector3 movePosition_;
     private float stepDistance_;
 
     // Start is called before the first frame update
-    void Start()
+    void Start()   // TODO: Все методы и поля желательно помечать модификатором доступа.
     {
         NextSideChange_ = Time.time;
         movePosition_ = transform.position;
@@ -50,7 +51,7 @@ public class PlayerScript : MonoBehaviour
 
     }
 
-    public void changeSide() 
+    public void changeSide()   // TODO: 1. Методы начинаются с заглавной буквы. 2. Стоит выделить логику выбора стороны в отдельный класс, к примеру ChangeSideController, и прокинуть ссылку здесь.
     {
         float BlockWidth = roadPrefab_.GetComponent<RoadParts>().GetBlockWidth();
         switch (side_)
